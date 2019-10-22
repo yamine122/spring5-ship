510200ac2f1aba33f7c3dc65f34db80b43faa166
wjdtmd92 <wjdtmd92@gmail.com>
2019-10-22 15:51:56 +0900

    191022

---
+++ src/main/java/com/ship/web/config/RootConfig.java
@@ -0,0 +1,37 @@
+package com.ship.web.config;
+
+import javax.sql.DataSource;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/*import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;*/
+
+@Configuration
+@MapperScan(basePackages = {"com.ship.web"})
+@ComponentScan(basePackages = {"com.ship.web"})
+public class RootConfig {
+	@Bean
+	public DataSource datasource() {
+		/*HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
+		hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/ship?serverTimezone=UTC");
+		hikariConfig.setUsername("ship");
+		hikariConfig.setPassword("ship");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		*/
+		
+		 DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		    dataSource.setUrl("jdbc:mysql://localhost:3306/ship?serverTimezone=UTC");
+		    dataSource.setUsername("ship");
+		    dataSource.setPassword("ship");
+		return dataSource;
+	}
+}
---
+++ src/test/java/com/ship/web/persistence/DataSourcesTest.java
@@ -0,0 +1,36 @@
+package com.ship.web.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.ship.web.config.RootConfig;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@ContextConfiguration(classes = {RootConfig.class})
+@Log4j
+public class DataSourcesTest {
+	@Setter(onMethod_ = {@Autowired })
+	
+	private DataSource dataSource;
+	
+	@Test
+	public void testConnection() {
+		try (Connection con = dataSource.getConnection()){
+			System.out.println("성공");
+			log.info(con);
+		} catch (Exception e) {
+			System.out.println("실패");
+			fail(e.getMessage());
+		}
+	}
+	
+}
